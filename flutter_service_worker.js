b019337d0c95525f6a170360661dccb04e1075d9
SubhagyaBorus <subhgyaborus@gmail.com>
2026-05-17 13:50:39 +0530

    update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "780d50f73e21e40f3b375a88c60171e0",
+const RESOURCES = {"flutter_bootstrap.js": "81f34745eabef7790cdda1a454174fab",
 "version.json": "bf902d2a6813a5e1841749c1424811c4",
-"index.html": "6a9f8417b720ef924ccf4d66b27363a6",
-"/": "6a9f8417b720ef924ccf4d66b27363a6",
-"main.dart.js": "17e64a421d22451f73058c46c9b19272",
+"index.html": "1e9246991874cd21f641d967ed61583a",
+"/": "1e9246991874cd21f641d967ed61583a",
+"main.dart.js": "b23a9515e9e28de61813abe852f3ed7d",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
